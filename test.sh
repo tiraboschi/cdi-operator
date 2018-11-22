1ecd7170a1ddfbf33ff55ca80638b4454631eb73
Simone Tiraboschi <stirabos@redhat.com>
2018-11-22 17:11:39 +0100

    Fix logs gathering

--- test.sh
+++ test.sh
@@ -4,6 +4,6 @@ make deploy
 # give everything time to come up - PLEASE FIX
 sleep 120
 # fetch operator logs
-oc logs -f $(oc get pods --selector name=cdi-operator --output=name)
+oc logs $(oc get pods --selector name=cdi-operator --output=name)
 # check for CDI related pods in kube-system ns
 oc get pod -o name --namespace=kube-system | grep cdi
